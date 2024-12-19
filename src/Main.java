8ff9684fdb799510f10292c516313b7f7a30a5f5
Deru <muzayzaymu@gmail.com>
2024-12-19 19:44:40 +0500

    library 2

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,7 @@ public class Main {
         Author Turgenev = new Author("Ivan", "Turgenev");
         Book FathersAndChildren = new Book("Fathers and children", 1862, Turgenev);
 
-        System.out.println(Idiot.getTitle() + " " + Idiot.getAuthor().getName() + " " + Idiot.getAuthor().getSurname() + " " + Idiot.getYearPublication());
-        System.out.println(FathersAndChildren.getTitle() + " " + FathersAndChildren.getAuthor().getName() + " " + FathersAndChildren.getAuthor().getSurname() + " " + FathersAndChildren.getYearPublication());
-
-        FathersAndChildren.setYearPublication(1865);
-        System.out.println("Измененая дата " + FathersAndChildren.getTitle() + " " + FathersAndChildren.getAuthor().getName() + " " + FathersAndChildren.getAuthor().getSurname() + " " + FathersAndChildren.getYearPublication());
-
+        System.out.println(Idiot);
+        System.out.println(FathersAndChildren);
     }
 }
